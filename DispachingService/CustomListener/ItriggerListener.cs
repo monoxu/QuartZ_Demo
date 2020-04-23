8a7d9019546e1fd642e9789cb26195ad311a5bc3
Xu <wangxu1186711291@gmail.com>
2020-04-23 09:53:36 +0800

    Last time

--- DispachingService/CustomListener/ItriggerListener.cs
+++ DispachingService/CustomListener/ItriggerListener.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using Quartz;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DispachingService.CustomListener
 {
-    class ItriggerListener
+    public class CustomTriggerListener : ITriggerListener
     {
+        public string Name => throw new NotImplementedException();
+
+        public Task TriggerComplete(ITrigger trigger, IJobExecutionContext context, SchedulerInstruction triggerInstructionCode, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TriggerFired(ITrigger trigger, IJobExecutionContext context, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TriggerMisfired(ITrigger trigger, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> VetoJobExecution(ITrigger trigger, IJobExecutionContext context, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
